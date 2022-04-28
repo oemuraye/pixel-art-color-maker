52f96d487e0ea8e26fa4bd5b8567496f24678fe3
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2022-04-28 15:08:44 +0100

    implemented color change/toggle

--- script.js
+++ script.js
@@ -3,8 +3,26 @@ const rowsNumber = document.getElementById('inputHeight')
 const columnNumber = document.getElementById('inputWidth')
 const pixelCanvas = document.getElementById('pixelCanvas')
 const form = document.getElementById('sizePicker')
-const tada = document.getElementsByTagName('td')
 
+// To create pixel art cells
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    pixelCanvas.innerHTML = ''
+    makeGrid()
+})
+
+// To add/remove color
+pixelCanvas.addEventListener('click', (e) => {
+    if (e.target.nodeName === 'TD') {
+        if (e.target.style.backgroundColor === '') { 
+            e.target.style.backgroundColor = colorPicker.value;
+        } else {
+            e.target.style.backgroundColor = '';
+        }
+    }
+})
+
+// To create cells/grid
 const makeGrid = () => {
     for (let i = 0; i < rowsNumber.value; i++) {
         const row = pixelCanvas.insertRow(0);
@@ -13,22 +31,3 @@ const makeGrid = () => {
         }        
     }
 }
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    pixelCanvas.innerHTML = ''
-    makeGrid()
-})
-
-pixelCanvas.addEventListener('click', (e) => {
-    if (e.target.nodeName === 'TD') {
-        if (e.target.backgroundColor = colorPicker.value) {
-          e.target.style.backgroundColor = "#fff";
-        } 
-    }
-})
-
-// tada.addEventListener('click', (e) => {
-//     if (e.target.backgroundColor = colorPicker.value) {
-//         e.target.style.backgroundColor = '#fff' 
-//     }
-// })
